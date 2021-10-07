e7afecdcd08f32d68bee4c46a44fb1adf7323e7b
harshtripathi24 <harshtripathistar@gmail.com>
2021-10-07 23:26:24 +0530

    Created Reusable Edited input Component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,20 @@
 /* eslint-disable arrow-body-style */
 import React from 'react'
-import { Button, Drawer, Icon } from 'rsuite'
+import { Button, Divider, Drawer, Icon } from 'rsuite'
 import { useProfile } from '../../context/profile.context'
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({onSignOut}) => {
 
     const {profile} = useProfile();
+
+    
+    // eslint-disable-next-line no-unused-vars
+    const onSave = async(newData) => {
+
+        console.log(newData);
+
+    }
 
     return (
         <>
@@ -16,6 +25,10 @@ const Dashboard = ({onSignOut}) => {
        </Drawer.Header>
        <Drawer.Body>
            <h3>Hey, {profile.name}</h3>
+           <Divider/>
+           <EditableInput  name="NickName" initialValue={profile.name} onSave={onSave} label={<h6 className="mb-2" >
+           Nickname
+           </h6>} />
 
        </Drawer.Body>
        <Drawer.Footer>
---
+++ src/components/EditableInput.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+import React, { useCallback, useState } from 'react'
+import { Alert, Icon, Input, InputGroup } from 'rsuite'
+
+const EditableInput = ({initialValue , onSave,label,name=null,placeholder='Write Your Value' ,emptyMsg="Input is Empty", ...inputProps}) => {
+    
+    const [input,setInput]  = useState(initialValue);
+    const [isEditable ,setIsEditable] = useState(false);
+    const onInputChange = useCallback((value)=> {
+        setInput(value);
+
+    },[])
+    const onEditClick = useCallback(()=>{
+        setIsEditable(editable=> !editable);
+        setInput(initialValue);
+    },[initialValue])
+
+    const onSaveClick = async()=>{
+        const trimmedValue = input.trim();
+
+        if(trimmedValue === ''){
+            Alert.info(emptyMsg,4000)
+        }
+        if (trimmedValue !== initialValue){
+            await onSave(trimmedValue);
+        } 
+
+        setIsEditable(false);
+
+    }
+    return (
+        <div>
+        {label}
+        <InputGroup>
+        <Input {...inputProps} disabled={!isEditable} placeholder={placeholder} value={input} onChange={onInputChange} />
+        <InputGroup.Button  onClick={onEditClick} >
+        
+        <Icon icon={isEditable? 'close' : 'edit2'} />
+        </InputGroup.Button>
+        {isEditable && <InputGroup.Button  onClick={onSaveClick} >
+        
+        <Icon icon='check' />
+        </InputGroup.Button>}
+        </InputGroup>
+        </div>
+    )
+}
+
+export default EditableInput
