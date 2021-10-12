d48966f71dace8bb80f1cbaf944cbee75de80f41
harshtripathi24 <harshtripathistar@gmail.com>
2021-10-12 23:13:23 +0530

    Created Room Context

---
+++ src/context/rooms.context.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { database } from '../misc/firebase';
+import { transformArrayWithId } from '../misc/Helper';
+
+const RoomContext = createContext();
+
+export const RoomProvider = ({ children }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [rooms, setRooms] = useState(null);
+
+  useEffect(() => {
+    const roomListRef = database.ref('rooms');
+
+    roomListRef.on(
+      'value',
+      snap => {
+        const data = transformArrayWithId(snap.val());
+        setRooms(data);
+
+        return () => {
+          roomListRef.off();
+        };
+      },
+      []
+    );
+  }, []);
+
+  return <RoomContext.Provider value={rooms}>{children}</RoomContext.Provider>;
+};
--- src/misc/Helper.js
+++ src/misc/Helper.js
@@ -1,10 +1,18 @@
- export  function getNameInitials(name){
+/* eslint-disable arrow-body-style */
+export function getNameInitials(name) {
+  const splitName = name.toUpperCase().split(' ');
 
-    const  splitName = name.toUpperCase().split(' ');
+  if (splitName.length > 1) {
+    return splitName[0][0] + splitName[1][0];
+  }
 
-    if(splitName.length > 1){
-        return splitName[0][0] + splitName[1][0];
-    }
+  return splitName[0][0];
+}
 
-    return splitName[0][0];
+export function transformArrayWithId(snapVal) {
+  return snapVal
+    ? Object.keys(snapVal).map(roomId => {
+        return { ...snapVal[roomId], id: roomId };
+      })
+    : [];
 }
