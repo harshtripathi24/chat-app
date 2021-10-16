26360c4a4b259050bd62a94f116a31c702c80e9e
harshtripathi24 <harshtripathistar@gmail.com>
2021-10-16 17:09:24 +0530

    displaed User Data with Modal Window

--- src/components/Chat-window/Messages/MessageItem.js
+++ src/components/Chat-window/Messages/MessageItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../ProfileAvatar';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 // eslint-disable-next-line arrow-body-style
 const MessageItem = ({ message }) => {
@@ -16,7 +17,11 @@ const MessageItem = ({ message }) => {
           size="xs"
         />
 
-        <span className="ml-2">{author.name}</span>
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        />
         <TimeAgo
           datetime={created}
           className="font-normal text-black-45 ml-2 "
