347881ebe9ec621d7c9bae14c8bf7157d87c8595
harshtripathi24 <harshtripathistar@gmail.com>
2021-10-17 13:23:06 +0530

    added Hover Hook for Messages

--- src/components/Chat-window/Messages/MessageItem.js
+++ src/components/Chat-window/Messages/MessageItem.js
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import { useHover } from '../../../misc/Custom-Hooks';
 import { auth } from '../../../misc/firebase';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
@@ -11,6 +12,8 @@ import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 const MessageItem = ({ message, handleAdmin }) => {
   const { author, created, text } = message;
 
+  const [selfRef, isHovered] = useHover();
+
   const isAdmin = useCurrentRoom(v => v.isAdmin);
   const admins = useCurrentRoom(v => v.admins);
 
@@ -19,7 +22,12 @@ const MessageItem = ({ message, handleAdmin }) => {
   const canGrantAdmin = isAdmin && !isAuthor;
 
   return (
-    <li className="padded mb-1">
+    <li
+      className={`padded mb-1 cursor-pointer ${
+        isHovered ? 'bg-black-02' : ''
+      } `}
+      ref={selfRef}
+    >
       <div className="d-flex align-items-center font-bolder mb-1">
         <PresenceDot uid={author.uid} />
 
